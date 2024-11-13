06fb2776a74f33a06d43bca6efbff34d2b904ef3
G_Rus <G_Rus@RUSHAN>
2024-11-14 02:23:32 +0300

    Added calendar for tasks deadlines

--- flask/app.py
+++ flask/app.py
@@ -1,25 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timedelta
+from collections import defaultdict
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.String(1000))
     complete = db.Column(db.Boolean)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime,default=datetime.now)
+    deadline_at = db.Column(db.DateTime,default=datetime.now)
 
 with app.app_context():
     db.create_all()
 
-@app.route("/", methods=["GET"])
+@app.get("/")
 def home():
     sort = request.args.get('sort', '')
     filter_by = request.args.get('filter', '')
@@ -48,7 +50,14 @@ def home():
 def add():
     title = request.form.get("title")
     description = request.form.get("description")
-    new_todo = Todo(title=title, description = description, complete=False)
+    try:
+        deadline_days = int(request.form.get("deadline_days"))
+    except ValueError:
+        flash('Please input correct data into fields')
+        return redirect(url_for("home"))
+
+    deadline_at = datetime.now() + timedelta(days=deadline_days)
+    new_todo = Todo(title=title, description = description, complete=False, deadline_at=deadline_at)
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("home"))
@@ -80,3 +89,13 @@ def delete(todo_id):
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("home"))
+
+@app.route("/calendar")
+def calendar():
+    tasks = Todo.query.all()
+    tasks_by_date = defaultdict(list)
+    for task in tasks:
+        task_date_str = task.deadline_at.strftime('%Y-%m-%d')
+        tasks_by_date[task_date_str].append(task.title)
+
+    return render_template("calendar.html", tasks_by_date=tasks_by_date)
